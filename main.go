f21badbb7b9e214df8671e2ecca106da14074d4d
skwangawur <rafiadw@gmail.com>
2026-09-06 18:58:12 +0700

    Revert "fix: security issues"
    This reverts commit 7e244b167836f20071be98ed8d27dc6b7d71d8da.

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -91,10 +90,10 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:              ":" + port,
-		ReadHeaderTimeout: 5 * time.Second,
-		Handler:           router,
+		Addr:    ":" + port,
+		Handler: router,
 	}
 
+	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
